45f540fe4bc8459865d67aa6af5c0ee3e51526b7
Kaikei-e <e_kaikei@protonmail.com>
2022-06-02 20:58:03 +0900

    Update: set up for chakra ui

--- node/app/suiibell-front/app/entry.client.tsx
+++ node/app/suiibell-front/app/entry.client.tsx
@@ -1,4 +1,34 @@
-import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
+// entry.client.tsx
+import React, { useState } from 'react'
+import { hydrate } from 'react-dom'
+import { CacheProvider } from '@emotion/react'
+import { RemixBrowser } from '@remix-run/react'
 
-hydrate(<RemixBrowser />, document);
+
+import createEmotionCache from './createEmotionCache'
+import { ClientStyleContext } from './context'
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode;
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = useState(createEmotionCache())
+
+  function reset() {
+    setCache(createEmotionCache())
+  }
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  )
+}
+
+hydrate(
+  <ClientCacheProvider>
+    <RemixBrowser />
+  </ClientCacheProvider>,
+  document,
+)
